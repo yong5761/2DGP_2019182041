bd367c52277aaa6a3068334a87c74bc1a23ab526
yong5761 <60994187+yong5761@users.noreply.github.com>
2026-09-22 14:37:10 +0900

    전체적인 움직임 구조 완성.원-사각형-삼각형 이동 기본 뼈대 마련

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,16 @@
 # 실습 과제 진행
+def move_circle():
+    pass
+
+def move_rectangle():
+    pass
+
+def move_triangle():
+    pass
+
+
 while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
     pass
