ddd55493d0ec44b1b734294cb7ba1a801a904ef7
yong5761 <60994187+yong5761@users.noreply.github.com>
2026-09-22 14:38:19 +0900

    lec05커밋

--- LEC05/character.py
+++ LEC05/character.py
@@ -4,14 +4,23 @@ from pico2d import *
 open_canvas(800, 600)
 
 # 여기를 채우시오.
+character = load_image('character.png')
+cx, cy = 400, 300
+radius = 200
+angle = 0
+start = True
 
+while start:
+    rad = math.radians(angle)
+    x = cx + radius * math.cos(rad)
+    y = cy + radius * math.sin(rad)
 
-
-
-
-
-
-delay(2)
+    clear_canvas()
+    character.draw(x, y)
+    update_canvas()
+    
+    angle = (angle + 1) % 360
+    delay(0.01)
 
 close_canvas()
 
